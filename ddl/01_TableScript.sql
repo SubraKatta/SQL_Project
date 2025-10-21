d847ae7e258999c3ec5a2dd685309fcd605cd267
SubraKatta <kattasubrahmanyamk@gmail.com>
2025-10-21 19:49:37 -0400

    First checkin New Table Creation

---
+++ ddl/01_TableScript.sql
@@ -0,0 +1,8 @@
+create table if not exists Employees (
+    EmployeeID int primary key,
+    FirstName varchar(50) not null,
+    LastName varchar(50) not null,
+    Email varchar(100) unique not null,
+    HireDate date not null,
+    Salary decimal(10, 2) check (Salary >= 0)
+);
